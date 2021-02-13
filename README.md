8f02f5473ae15972a300249194561027906bc011
mosugi <mosuging@gmail.com>
2021-02-13 18:22:16 +0900

    Refactor & Write README.md

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # mshare
+
+This is a command line tool to share text/url to Notes/Message/Email/Airdrop from your terminal.
+
+## Installation
+
+```
+git clone https://github.com/mosugi/mshare.git
+cd mshare
+make install
+```
+
+## Usage
+
+### Share to Notes
+
+```
+mshare notes "Hello World"
+mshare notes https://google.com
+```
+
+### Share to Message
+
+```
+mshare message "Hello World"
+mshare message https://google.com
+```
+
+### Share to Email
+
+```
+mshare email "Hello World"
+mshare email https://google.com
+```
+
+### Share to AirDrop
+
+```
+mshare airdrop https://google.com
+```
+
+## Thanks
+
+https://github.com/vldmrkl/airdrop-cli
+https://gist.github.com/tearfulDalvik/e656177d3df7521cd61dffdc24ef3ec3
+https://qiita.com/hcrane/items/e784a5f7c4fb5e6470e6
--- Sources/mshare/main.swift
+++ Sources/mshare/main.swift
@@ -14,51 +14,119 @@ let app = NSApplication.shared
 app.delegate = mShare
 app.run()
 
+//Thanks! https://github.com/vldmrkl/airdrop-cli
 class MShare:NSObject,NSApplicationDelegate,NSSharingServiceDelegate{
-    
-    let sharingServiceName = [
-        "notes": NSSharingService.Name(rawValue: "com.apple.Notes.SharingExtension"),
-        "message": NSSharingService.Name.composeMessage,
-        "email": NSSharingService.Name.composeEmail,
-        "airdrop": NSSharingService.Name.sendViaAirDrop,
-        // dont work
-        // "reminders" : NSSharingService.Name(rawValue: "com.apple.reminders.RemindersShareExtension")
-    ]
-    
+        
     func applicationDidFinishLaunching(_ aNotification: Notification){
-        let argc = Int(CommandLine.argc)
-        guard argc >= 3 else {exit(0)}
+
+        if CommandLine.argc == 3 {
+            callFromTerminal()
+        } else if CommandLine.argc == 2 && CommandLine.arguments[1].hasPrefix("chrome-extension") {
+            callFromChrome()
+        } else {
+            exit(2)
+        }
+        
+    }
+    
+    func callFromTerminal() {
         
         let serviceName = CommandLine.arguments[1]
         let text = CommandLine.arguments[2]
         
-        guard let service = sharingServiceName[serviceName]
+        share(service:sharingService(name: serviceName),withItem: text)
+    }
+    
+    func callFromChrome() {
+        
+        guard let req = chromeRead()
         else{
             exit(2)
         }
         
-        share(service:service,withItem: text)
+        let json = req.data(using: .utf8)
+        let message = try! JSONDecoder().decode(ChromeExtensionMessage.self, from: json!)
+        
+        share(service:sharingService(name: message.service),withItem: message.text)
+        
     }
     
-    func share(service:NSSharingService.Name,withItem:String){
-        guard let sharingService: NSSharingService = NSSharingService(named: service)
+    func sharingService(name:String) -> NSSharingService{
+        
+        let sharingServiceName = [
+            "notes": NSSharingService.Name(rawValue: "com.apple.Notes.SharingExtension"),
+            "message": NSSharingService.Name.composeMessage,
+            "email": NSSharingService.Name.composeEmail,
+            "airdrop": NSSharingService.Name.sendViaAirDrop,
+            // dont work
+            // "reminders" : NSSharingService.Name(rawValue: "com.apple.reminders.RemindersShareExtension")
+        ]
+        
+        guard let sharingService: NSSharingService = NSSharingService(named: sharingServiceName[name]!)
         else{
             exit(2)
         }
+        
+        return sharingService
+    }
+    
+    func share(service:NSSharingService,withItem:String){
         
         let shareItem: Any = withItem.isUrl ? NSURL(string: withItem) as Any : withItem
         
-        if sharingService.canPerform(withItems: [shareItem]){
-            sharingService.delegate = self
-            sharingService.perform(withItems: [shareItem])
+        if service.canPerform(withItems: [shareItem]){
+            service.delegate = self
+            service.perform(withItems: [shareItem])
         }else{
             exit(1)
         }
-        
     }
     
 }
 
+struct ChromeExtensionMessage: Codable {
+    let service: String
+    let text: String
+}
+
+// Thanks! https://gist.github.com/tearfulDalvik/e656177d3df7521cd61dffdc24ef3ec3
+func getInt(_ bytes: [UInt]) -> UInt {
+    let lt = (bytes[3] << 24) & 0xff000000
+    let ls = (bytes[2] << 16) & 0x00ff0000
+    let lf = (bytes[1] << 8) & 0x0000ff00
+    let lz = (bytes[0] << 0) & 0x000000ff
+    return lt | ls | lf | lz
+}
+
+func getIntBytes(for length: Int) -> [UInt8] {
+    var bytes = [UInt8](repeating: 0, count: 4)
+    bytes[0] = UInt8((length & 0xFF));
+    bytes[1] = UInt8(((length >> 8) & 0xFF));
+    bytes[2] = UInt8(((length >> 16) & 0xFF));
+    bytes[3] = UInt8(((length >> 24) & 0xFF));
+    return bytes
+}
+
+func chromeRead() -> String? {
+    let stdIn = FileHandle.standardInput
+    var bytes = [UInt](repeating: 0, count: 4)
+    guard read(stdIn.fileDescriptor, &bytes, 4) != 0 else {
+        return nil
+    }
+    
+    let len = getInt(bytes)
+    return String(data: stdIn.readData(ofLength: Int(len)), encoding: .utf8)
+}
+
+func chromeWrite(_ txt: String) {
+    let stdOut = FileHandle.standardOutput
+    
+    let len = getIntBytes(for: txt.utf8.count)
+    stdOut.write(Data(bytes: len, count: 4))
+    stdOut.write(txt.data(using: .utf8)!)
+}
+
+// Thanks! https://qiita.com/hcrane/items/e784a5f7c4fb5e6470e6
 extension String {
     var isUrl: Bool {
         let linkValidation = NSTextCheckingResult.CheckingType.link.rawValue
